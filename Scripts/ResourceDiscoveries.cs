4cd5f7c8206d2048151b0ae88186e06fb5ffc536
Christopher Driscoll <cdrisco@users.noreply.github.com>
2023-12-19 18:10:10 -0600

    Capturing Resource Discoveries
    Each Resource Discovery has its own CaptureTimer that gets enabled if within range.
    ResourceDiscoveries turn color when captured
    ResourceDiscoveries are more evenly spaced out and don't overlap

--- Scripts/ResourceDiscoveries.cs
+++ Scripts/ResourceDiscoveries.cs
@@ -5,10 +5,6 @@ using System.Diagnostics;
 public partial class ResourceDiscoveries : Node2D
 {
 
-//private PackedScene resourceDiscoveryTemplate = (PackedScene)GD.Load("res://Scenes/resource_template.tscn");
-//private PackedScene resourceDiscoveryTemplate = (PackedScene)GD.Load("ResourceDiscoveries/IronMine.tres");
-//RDResource=(ResourceDiscoveryResource)ResourceLoader.Load("ResourceDiscoveries/IronMine.tres").Duplicate(true);
-
 [Export] private PackedScene rTemplateIronMine;
 [Export] private PackedScene rTemplateGoldMine;
 [Export] private PackedScene rTemplateManaWell;
@@ -16,13 +12,14 @@ public partial class ResourceDiscoveries : Node2D
 
 [Export] public int gridSizeX;
 [Export] public int gridSizeY;
-[Export] public int cellSizeX;
-[Export] public int cellSizeY;
+[Export] public static int cellSizeX=1920;
+[Export] public static int cellSizeY=1080;
 [Export] public int resourcesPerCell;
 
 private Node2D resourceDiscovery;
+private Node2D capTimer;
 
-	public override void _Ready()
+    public override void _Ready()
 	{
 		GD.Randomize();
 		//Place resource discoveries
@@ -35,7 +32,6 @@ private Node2D resourceDiscovery;
 
 private void PlaceResourceDiscoveries()
 {
-	var resourceDiscoveries = new System.Collections.Generic.List<Node>();
 	uint rdType;
 	int py=-(gridSizeY/2);
 	int py2=gridSizeY/2;
@@ -47,57 +43,47 @@ private void PlaceResourceDiscoveries()
 		for (int x=-(gridSizeX/2);x<=gridSizeX/2;x++)
 			for (int i=1;i<=resourcesPerCell;i++)
 			{
-				
-				rdType=GD.Randi() % 4;	
+					Debug.Print("RD Len: " + GetChildCount());
+                Vector2 pos = GetRandomPos();
+                rdType =GD.Randi() % 4;
 
-				switch (rdType)
+                switch (rdType)
 				{
 					case 0:
 						resourceDiscovery = (Node2D)rTemplateIronMine.Instantiate();
-						Debug.Print("rTemplateIronMine");
 						break;
 					case 1:
 						resourceDiscovery = (Node2D)rTemplateGoldMine.Instantiate();
-						Debug.Print("rTemplateGoldMine");
 						break;
 					case 2:
 						resourceDiscovery = (Node2D)rTemplateManaWell.Instantiate();
-						Debug.Print("rTemplateManaWell");
 						break;
 					case 3:
 						resourceDiscovery = (Node2D)rTemplateWood.Instantiate();
-						Debug.Print("rTemplateWood");
 						break;
 				}
 
-				resourceDiscoveries.Add(resourceDiscovery);
 				AddChild(resourceDiscovery);
 
-				Vector2 pos=GetRandomPos();
+
 				resourceDiscovery.Position=new Vector2(x*cellSizeX+pos.X,y*cellSizeY+pos.Y);
+                ResourceDiscovery rdp = (ResourceDiscovery)GetNode(resourceDiscovery.GetPath());
 
-				ResourceDiscovery rd=(ResourceDiscovery)GetNode(resourceDiscovery.GetPath());
+					string myType = rdp.RDResource.resourceType.ToString();
 
-				Debug.Print("pos:"+resourceDiscovery.Position+" type:"+rd.RDResource.recoverType);
+                    Debug.Print("pos:"+resourceDiscovery.Position+" type:"+ myType);
 				
 			}
 
-		//Sprite2D n = GetNode<Sprite2D>("ResourceTemplate");
-		//n.Position=new Vector2(345,167);
-
-		Debug.Print("Resources: "+resourceDiscoveries.Count);
 	}
 
-	private Vector2 GetRandomPos()
+	public static Vector2 GetRandomPos()
 	{
-		// get random x and y pos within the cell
-		float xPos=(float)GD.Randi() % cellSizeX;
-		float yPos=(float)GD.Randi() % cellSizeY;
-
-// TODO: repeat this until it gets a pos that is at least a certain minimum distance away from all other resources
+			// get random x and y pos within the cell
+			float xPos = (float)GD.Randi() % cellSizeX;
+			float yPos = (float)GD.Randi() % cellSizeY;
 
 		return new Vector2(xPos,yPos);
-
 	}
 
 }
