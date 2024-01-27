e570235292cb2daceaecfce3227bddc1c15335a4
Christopher Driscoll <cdrisco@users.noreply.github.com>
2024-01-27 17:22:55 -0600

    Fixed resource collection and GUI
    It looks like there was a missing signal that was causing the issues

--- Scripts/ResourceDiscoveries.cs
+++ Scripts/ResourceDiscoveries.cs
@@ -5,39 +5,101 @@ using System.Diagnostics;
 public partial class ResourceDiscoveries : Node2D
 {
 
-[Export] private PackedScene rTemplateIronMine;
-[Export] private PackedScene rTemplateGoldMine;
-[Export] private PackedScene rTemplateManaWell;
-[Export] private PackedScene rTemplateWood;
-[Export] public int gridSizeX;
-[Export] public int gridSizeY;
-[Export] public static int cellSizeX=1920;
-[Export] public static int cellSizeY=1080;
-[Export] public int resourcesPerCell;
+	[Export] private PackedScene rTemplateIronMine;
+	[Export] private PackedScene rTemplateGoldMine;
+	[Export] private PackedScene rTemplateManaWell;
+	[Export] private PackedScene rTemplateWood;
+	[Export] public int gridSizeX;
+	[Export] public int gridSizeY;
+	[Export] public static int cellSizeX=1920;
+	[Export] public static int cellSizeY=1080;
+	[Export] public int resourcesPerCell;
 
-private Node2D resourceDiscovery;
-private Node2D capTimer;
+// resource timer
+	[Export] public int resourceUpdateFreq;
+	private int curResourceTimer=0;
 
+	private Node2D resourceDiscovery;
+	private Node2D capTimer;
+
+// resource tracking
+	private int gold; // how much gold you have
+	static private int goldResourceCount; // how many gold mine resources you have discovered
+	private int iron;
+	private static int ironResourceCount;
+	private int mana;
+	private static int manaResourceCount;
+	private int wood;
+
+    private int seconds = 0;
+	private int minutes = 0;
+
+// GUI node
+	private Node rGUI;
+	private resourceGUI rG2;
 	public override void _Ready()
 	{	
 
 		GD.Randomize();
 		//Place resource discoveries
 		PlaceResourceDiscoveries();	
-	}
 
-	void Reparent(Node n)
-	{
-		n.GetParent().RemoveChild(n);
-		AddChild(n);
+		// get GUI nodes
+		rGUI = GetNode("../GUI");
+        rG2 = (resourceGUI)GetNode(rGUI.GetPath());
+        UpdateResourceGUI();
 	}
-
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
 
+// Game Timer - also updates resources every minute
+	public void OnTimer()
+	{
+		seconds++;
+		if (seconds>59)
+		{
+			seconds = 0;
+			minutes++;
+		}
+
+		string strSeconds = seconds.ToString();
+		if (seconds < 10)
+			strSeconds = "0" + strSeconds;
+
+		string strMinutes = minutes.ToString();
+
+        if (minutes < 10)
+            strMinutes = "0" + strMinutes;
+
+		
+
+		rG2.lblTimer.Text = strMinutes+":"+strSeconds;
+
+		// update resources
+		curResourceTimer++;
+		if (curResourceTimer>=resourceUpdateFreq)
+		{
+			curResourceTimer = 0;
+            gold += goldResourceCount;
+            iron += ironResourceCount;
+            mana += manaResourceCount;
+            //wood += woodResourceCount;
+            UpdateResourceGUI(); 
+		}
+
+        Debug.Print("Timer: " + minutes+":"+seconds);
+	}
+
+	private void UpdateResourceGUI()
+	{
+		rG2.lblGold.Text = gold.ToString();
+        rG2.lblIron.Text = iron.ToString();
+        rG2.lblMana.Text = mana.ToString();
+        rG2.lblWood.Text = wood.ToString();
+    }
 private void PlaceResourceDiscoveries()
 {
 	uint rdType;
@@ -95,5 +157,22 @@ private void PlaceResourceDiscoveries()
 
 		return new Vector2(xPos,yPos);
 	}
+
+	// this gets called from the apropriate ResourceDiscovery.cs
+		public static void AddRD(string resourceType,int amount) 
+	{
+		if (resourceType=="Gold")
+		{
+            goldResourceCount += amount;
+		}
+        if (resourceType == "Iron")
+        {
+            ironResourceCount += amount;
+        }
+        if (resourceType == "Mana")
+        {
+            manaResourceCount += amount;
+        }
+    }
 
 }
